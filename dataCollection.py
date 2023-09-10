7fff8296986a9ea8de969c3221b9ef7e2355745b
Nimun Bajwa <nimun2001@gmail.com>
2023-09-10 15:44:22 -0400

    Dection, Cropping, Overlay done

--- dataCollection.py
+++ dataCollection.py
@@ -1,5 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
 
 # Step One: Webcam
 # Step Two: Detect Hand
@@ -7,8 +9,40 @@ from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)  # Capture object - default camera (built-in webcam) should be used.
 detector = HandDetector(maxHands=1)
+
+offset = 20
+imgSize = 300
 while True:
     success, img = cap.read()  # Capture a frame from the webcam
-    hands, img = detector.findHands(img) # Find Hands
+    hands, img = detector.findHands(img)  # Find Hands
+    if hands:
+        hand = hands[0]  # Note there is only one hand
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255  # Colored image - 3 values (r,g,b)
+        imgCrop = img[y - offset:y + h + offset, x - offset: x + w + offset]  # Bounding Box
+
+        aspectRatio = h / w
+
+        # Overlay image on top of white image - maximizing space of square it takes
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize  # Overlay
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize  # Overlay
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
     cv2.imshow("Image", img)  # Display the captured frame
     cv2.waitKey(1)  # 1 second delay
